97432a1e105b37178d5df82e5b2a1f39ef27c38b
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-15 15:36:41 +0500

    feat: add functions

--- CS50x-PYTHON/hello.c
+++ CS50x-PYTHON/hello.c
@@ -1,7 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string answer = get_string("What's you name: ");
+    printf("hello, %s\n", answer);
     return 0;
 }
